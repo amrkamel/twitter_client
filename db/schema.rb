4c4b5303b09934ed10cd801eeadfabef7e797123
Amr Kamel <3amr.kaml@gmail.com>
2015-07-02 11:49:57 +0200

    Create countries table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150702091528) do
+ActiveRecord::Schema.define(version: 20150702094428) do
+
+  create_table "countries", force: :cascade do |t|
+    t.string   "code",       limit: 255
+    t.string   "name",       limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
