aba198c49c54e2b92f3bf46a9df725e568ffdd66
Amr Kamel <3amr.kaml@gmail.com>
2015-07-02 12:36:31 +0200

    Add gravatar url to users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  # with the next two lines you can directly get any user gravatar url through user.gravatar_url
+  include Gravtastic
+  gravtastic
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
